d1d2ab9556455ab8c2e7b46d30fac9a182f4562f
sheche24 <wlsrmssla@naver.com>
2021-07-03 22:56:19 +0900

    django DetailView

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from accountapp.views import AccountCreateView, hello_world
+from accountapp.views import AccountCreateView, AccountDetailView, hello_world
 
 # 이름 변경 → url에서는 account 이지만, 파이썬 상에서는 accountapp으로 사용
 app_name = "accountapp"
@@ -13,6 +13,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 
-    path('create/', AccountCreateView.as_view(), name='create')   # 클래스 베이스 형 → 이름 뒤에 .as_view() 붙여야함
-
+    path('create/', AccountCreateView.as_view(), name='create'),   # 클래스 베이스 형 → 이름 뒤에 .as_view() 붙여야함
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),    # primary key로 받음
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse, reverse_lazy
 from accountapp.models import HelloWorld
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -32,3 +32,9 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')    # reverse는 class에서 사용할 수 없음(함수에서 사용). 해당주소로 렌더링
     template_name = 'accountapp/create.html'
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'         # html에서 사용하는 변수 이름 설정
+    template_name = 'accountapp/detail.html'
