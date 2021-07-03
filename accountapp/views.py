2d8ab6b546896c228777f2460fd240c718dc4971
sheche24 <wlsrmssla@naver.com>
2021-07-03 12:52:33 +0900

    django html

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'base.html')
+    # return HttpResponse('Hello World!')
