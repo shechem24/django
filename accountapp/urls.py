75a23842faabcdd4782f6468908f95854be75b4e
sheche24 <wlsrmssla@naver.com>
2021-07-03 20:56:07 +0900

    django lonin&logout

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from django.urls import path, include
-from accountapp.views import hello_world
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from accountapp.views import AccountCreateView, hello_world
 
+# 이름 변경 → url에서는 account 이지만, 파이썬 상에서는 accountapp으로 사용
 app_name = "accountapp"
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),   # 함수형
+
+    # 장고 기본 제공하는 login/logout view 바로 사용 가능
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('create/', AccountCreateView.as_view(), name='create')   # 클래스 베이스 형 → 이름 뒤에 .as_view() 붙여야함
+
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,34 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse, reverse_lazy
+from accountapp.models import HelloWorld
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
+# 함수형 view
 def hello_world(request):
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={ 'text': 'POST METHOD!!!' })
+        temp = request.POST.get('hello_world_input')
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+        hello_world_list = HelloWorld.objects.all()
+
+        # return render(request, 'accountapp/hello_world.html', context={ 'hello_world_list': hello_world_list })
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))  # reverse: 경로 만들기
     else:
-        return render(request, 'accountapp/hello_world.html', context={ 'text': 'GET METHOD!!!' })
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={ 'hello_world_list': hello_world_list })
+
+
+# class 형 view
+# CreateView: 장고 기본 제공하는 가입 양식
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')    # reverse는 class에서 사용할 수 없음(함수에서 사용). 해당주소로 렌더링
+    template_name = 'accountapp/create.html'
