9495c116112eec85c3c16d94ea0c3610125e4b09
sheche24 <wlsrmssla@naver.com>
2021-07-03 13:55:23 +0900

    django use css

--- django_contigo/settings.py
+++ django_contigo/settings.py
@@ -133,6 +133,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",    # static 파일 경로
+    ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
